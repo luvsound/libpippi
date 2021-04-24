f6894deea65f7d36d40f2ec1cff1b2d34f46301c
Erik Schoster <erik@hecanjog.com>
2021-04-23 23:24:49 -0500

    Add hermite interpolation

--- src/interpolation.h
+++ src/interpolation.h
@@ -6,6 +6,8 @@
 typedef struct interpolation_factory_t {
     lpfloat_t (*linear_pos)(buffer_t*, lpfloat_t);
     lpfloat_t (*linear)(buffer_t*, lpfloat_t);
+    lpfloat_t (*hermite_pos)(buffer_t*, lpfloat_t);
+    lpfloat_t (*hermite)(buffer_t*, lpfloat_t);
 } interpolation_factory_t;
 
 extern const interpolation_factory_t Interpolation;
